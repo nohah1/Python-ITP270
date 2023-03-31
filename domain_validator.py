bd1783785b6466cd1276da55cfc16395808e49be
Cyber Range Student <student@ip-10-1-68-32.ec2.internal>
2023-03-31 01:12:08 +0000

    Adding python domain name script

---
+++ domain_validator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import whois # pip3 install python-whois
+
+def is_registered(domain_name):
+	"""A function that returns a boolean indicating 
+	whether a 'domain_name' is registered"""
+	try:
+		w = whois.whois(domain_name)
+	except Exception:
+		return False
+	else:
+		return bool(w.domain_name)
+
+if __name__ == "__main__":
+	print(is_registered("google.com"))
+	print(is_registered("something-that-do-not-exist.com"))
+
